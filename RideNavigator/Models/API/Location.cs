11ca7811f27451d6555e5679961a23b0b559033a
Justin Bentley <justinmbentley@outlook.com>
2022-11-30 15:43:01 +1100

    Location
    Note: ipinfo.io Key withheld

---
+++ RideNavigator/Models/API/Location.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace RideNavigator.Models.API
+{
+
+    public class Location
+    {
+        public class JsonResponse
+        {
+            public string? city { get; set; }
+
+            //below can be uncommented for a more detailed response
+            //public string? region { get; set; }
+            //public string? country { get; set; }
+            //public string? loc { get; set; }
+            //public string? org { get; set; }
+            //public string? timezone { get; set; }
+            //public string? ip { get; set; }
+        }
+
+        public async Task<string>? Get(string ip)
+        {
+
+            if (ip.Contains("localhost")) { ip = "123.123.123.123"; }
+
+            var url = $"https://ipinfo.io/";
+
+            var token = Environment.GetEnvironmentVariable("IPINFO_KEY");
+
+            var parameters = $"{ip}?token={token}";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync(parameters).ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                JsonResponse? jsonList = JsonSerializer.Deserialize<JsonResponse>(jsonString);
+
+                return JsonSerializer.Serialize(jsonList);
+            }
+            else
+            {
+                return null!;
+            }            
+
+        }
+
+    }
+}
