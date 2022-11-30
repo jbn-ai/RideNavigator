4957187f67871a55ca258f84379d4066bb1b2566
Justin Bentley <justinmbentley@outlook.com>
2022-11-30 14:25:40 +1100

    Candidate

---
+++ RideNavigator/Models/API/Candidate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace RideNavigator.Models.API
+{
+
+    public class Candidate
+    {       
+        public string Get()
+        {
+
+            var data = new
+            {
+                name = "test",
+                phone = "test"
+            };
+
+            return JsonSerializer.Serialize(data);
+
+        }
+
+    }
+
+}
